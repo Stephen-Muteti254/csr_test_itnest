37d6a642aea005ebb6cfe427472055f0bd7da650
Stephen Muteti <stephenmuteti09@gmail.com>
2026-04-23 03:11:01 +0300

    Initial commit

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,9 @@
-// main.tsx
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "@tanstack/react-router";
-import { router } from "./router";
+import { getRouter } from "./router";
+
+const router = getRouter();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <RouterProvider router={router} />
